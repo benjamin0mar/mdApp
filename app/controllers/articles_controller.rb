4b019eee2f8850b0e8cf5c285249573e897476d6
Omar Benjamin <benjamin0mar@hotmail.com>
2017-02-21 23:02:22 -0500

    added localization datetime

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,14 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_locale
   # GET /articles
   # GET /articles.json
+
+
+  def set_locale
+    I18n.locale = params[:locale].to_sym
+  end
+
   def index
     @articles = Article.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
-  root "articles#index"
+  
+  scope '/(:locale)', defaults: { locale: 'en' }, constraints: { locale: /en|es/ } do
+
+      resources :articles
+      root "articles#index"
+
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
